031b4eb1a6cceb1d7d6626e00e92da785c36656b
liuming <liuming@wuji.co>
2022-05-05 16:45:41 +0800

    delete 测试代码

--- ios/Classes/Base/RtcEngine.swift
+++ ios/Classes/Base/RtcEngine.swift
@@ -973,11 +973,8 @@ class RtcEngineManager: NSObject, RtcEngineInterface {
     }
     
     @objc func startMediaRecord(_ params: NSDictionary, _ callback: Callback) {
-        
         callback.resolve(engine) { it in
-            let documentPath2 = NSHomeDirectory() + "/Documents/test.m3u8"
             var config = params["mediaRecordConfig"] as! NSMutableDictionary
-            config["fileName"] = documentPath2
             let jsonDic:NSDictionary = ["media_record_start":config]
             if let data = try? JSONSerialization.data(withJSONObject: jsonDic, options: .prettyPrinted) {
                 if let jsonString = String(data: data, encoding: . utf8) {
